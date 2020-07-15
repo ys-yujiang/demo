0ed74b5503e350a850419fa4ee2062f860950a60
ys-yujiang <64036718+ys-yujiang@users.noreply.github.com>
2020-07-15 12:13:20 -0700

    update

--- AWS/main.tf
+++ AWS/main.tf
@@ -33,6 +33,8 @@ resource "aws_instance" "machine1" {
 #resource "aws_instance" "machine2" {
 #    ami           = "ami-04b9e92b5572fa0d1"
 #    instance_type = "t2.micro"
-#    availability_zone = "us-east-1b"
+#    availability_zone = "us-east-1b" 
+#    adding commit
+
 #
 #}
