4e83c32d0906b4a917a96a157022003d28b3cb70
ys-yujiang <64036718+ys-yujiang@users.noreply.github.com>
2020-08-04 11:51:00 -0700

    new

--- AWS/main.tf
+++ AWS/main.tf
@@ -11,24 +11,36 @@ variable "department" {
   description = "Department tag"
 }
 
-resource "random_string" "random" {
-    length = 16
-    special = true
-    override_special = "/@£$"
-
-    provisioner "local-exec" {
-       command = "echo sleeping for 10 mins; sleep 600; echo slept"
-    }
+resource "aws_volume_attachment" "ebs_att" {
+  device_name = "/dev/sdh"
+  volume_id   = aws_ebs_volume.example.id
+  instance_id = aws_instance.web.id
 }
 
-resource "aws_instance" "machine1" {
-    depends_on = [
-        random_string.random,
-       ]
-    ami           = "ami-04b9e92b5572fa0d1"
-    instance_type = "t2.micro"
-    availability_zone = "us-east-1b"
-    count = 3
+resource "aws_instance" "web" {
+  ami               = "ami-21f78e11"
+  availability_zone = "us-east-1"
+  instance_type     = "t1.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+resource "aws_ebs_volume" "example" {
+  availability_zone = "us-east-1"
+  size              = 1
+}
+
+resource "aws_network_interface" "test" {
+  subnet_id       = "${aws_subnet.public_a.id}"
+  private_ips     = ["10.0.0.50"]
+  security_groups = ["${aws_security_group.web.id}"]
+
+  attachment {
+    instance     = "${aws_instance.test.id}"
+    device_index = 1
+  }
 }
 
 #resource "aws_instance" "machine2" {
